e6cb24fe0656b56beb88bdad18feebe39105ccb9
AlexCiobanu47 <alexciobanu72@yahoo.com>
2023-05-10 12:08:24 +0300

    added mongodb connection and middleware

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+
+//express app
+const app = express();
+//middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.next, req.method);
+  next();
+});
+// connect to database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(
+        "Connected to the database and listening on port: ",
+        process.env.PORT
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
