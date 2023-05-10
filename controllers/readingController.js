74755cc5e0f0bde1fd869c4d0398fdc80b80e115
AlexCiobanu47 <alexciobanu72@yahoo.com>
2023-05-10 16:07:57 +0300

    added routes and controllers for humidity, light, gas and motion

--- controllers/readingController.js
+++ controllers/readingController.js
@@ -11,5 +11,26 @@ const getAllTemperatures = async (req, res) => {
   const temperatures = await Temperature.find({}).sort({ createdAt: -1 });
   res.status(200).json(temperatures);
 };
-
-module.exports = { getAllTemperatures };
+const getAllHumidities = async (req, res) => {
+  const humidities = await Humidity.find({}).sort({ createdAt: -1 });
+  res.status(200).json(humidities);
+};
+const getAllLights = async (req, res) => {
+  const lights = await Light.find({}).sort({ createdAt: -1 });
+  res.status(200).json(lights);
+};
+const getAllGasConcentrations = async (req, res) => {
+  const concentrations = await Gas.find({}).sort({ createdAt: -1 });
+  res.status(200).json(concentrations);
+};
+const getAllMotionDetections = async (req, res) => {
+  const detections = await Motion.find({}).sort({ createdAt: -1 });
+  res.status(200).json(detections);
+};
+module.exports = {
+  getAllTemperatures,
+  getAllHumidities,
+  getAllLights,
+  getAllGasConcentrations,
+  getAllMotionDetections,
+};
--- routes/readingRoutes.js
+++ routes/readingRoutes.js
@@ -6,9 +6,19 @@ const Light = require("../models/lightsModel");
 const Gas = require("../models/gasModel");
 const Motion = require("../models/motionModel");
 
-const { getAllTemperatures } = require("../controllers/readingController");
+const {
+  getAllTemperatures,
+  getAllHumidities,
+  getAllLights,
+  getAllGasConcentrations,
+  getAllMotionDetections,
+} = require("../controllers/readingController");
 
 const router = express.Router();
 router.get("/temp", getAllTemperatures);
+router.get("/hum", getAllHumidities);
+router.get("/light", getAllLights);
+router.get("/gas", getAllGasConcentrations);
+router.get("/motion", getAllMotionDetections);
 
 module.exports = router;
